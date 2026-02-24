65ca08a30f8701d2b7e83014c43f8cf5424feb8a
yessiq <2146174683@qq.com>
2026-02-24 14:54:11 +0800

    2.0.3 release

--- src/main/java/moddedmite/waila/handlers/emi/EMIHandler.java
+++ src/main/java/moddedmite/waila/handlers/emi/EMIHandler.java
@@ -1,26 +1,25 @@
 package moddedmite.waila.handlers.emi;
 
-import net.minecraft.Block;
+import mcp.mobius.waila.api.impl.DataAccessorCommon;
 import net.minecraft.ItemStack;
-import net.minecraft.Minecraft;
 
 import java.util.Objects;
 
 public class EMIHandler {
-    static Minecraft mc = Minecraft.getMinecraft();
+    static DataAccessorCommon accessor = DataAccessorCommon.instance;
 
     public static dev.emi.emi.api.stack.EmiStack updateEmiStack() {
-        return dev.emi.emi.api.stack.EmiStack.of(new ItemStack(Block.blocksList[mc.theWorld.getBlockId(mc.objectMouseOver.block_hit_x, mc.objectMouseOver.block_hit_y, mc.objectMouseOver.block_hit_z)]));
+        return dev.emi.emi.api.stack.EmiStack.of(new ItemStack(accessor.block));
     }
 
     public static void displayRecipes() {
-        if (mc.objectMouseOver != null && mc.objectMouseOver.isBlock()) {
+        if (accessor != null && accessor.getBlock() != null) {
             dev.emi.emi.api.EmiApi.displayRecipes(Objects.requireNonNull(EMIHandler.updateEmiStack()));
         }
     }
 
     public static void displayUses() {
-        if (mc.objectMouseOver != null && mc.objectMouseOver.isBlock()) {
+        if (accessor != null && accessor.getBlock() != null) {
             dev.emi.emi.api.EmiApi.displayUses(Objects.requireNonNull(EMIHandler.updateEmiStack()));
         }
     }
