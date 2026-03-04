35fed8e0c6ac852fb9091e612b241dd17b794075
Inf1nlty <60602644+Inf1nlty@users.noreply.github.com>
2026-03-04 22:04:59 +0800

    Tile entity nbt fix

--- src/main/java/mcp/mobius/waila/network/WailaPacketHandler.java
+++ src/main/java/mcp/mobius/waila/network/WailaPacketHandler.java
@@ -2,6 +2,7 @@ package mcp.mobius.waila.network;
 
 import mcp.mobius.waila.Waila;
 import mcp.mobius.waila.WailaExceptionHandler;
+import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.api.impl.DataAccessorCommon;
 import mcp.mobius.waila.api.impl.ModuleRegistrar;
@@ -55,18 +56,52 @@ public class WailaPacketHandler {
                 {
                     Packet0x01TERequest castedPacket = new Packet0x01TERequest(packet);
                     MinecraftServer server = MinecraftServer.getServer();
-                    TileEntity entity = server.worldServers[castedPacket.worldID].getBlockTileEntity(castedPacket.posX, castedPacket.posY, castedPacket.posZ);
+                    World teWorld = null;
 
-                    if (entity instanceof TileEntityFurnace)
+                    for (World w : server.worldServers)
                     {
-                        if ((entity instanceof TileEntitySkull))
+                        if (w != null && w.provider.dimensionId == castedPacket.worldID)
                         {
-                            return;
+                            teWorld = w;
+                            break;
                         }
+                    }
+
+                    if (teWorld == null) return;
+
+                    TileEntity entity = teWorld.getBlockTileEntity(castedPacket.posX, castedPacket.posY, castedPacket.posZ);
+
+                    if (entity == null) return;
+
+                    if (entity instanceof TileEntitySkull) return;
+
+                    Block teBlock = teWorld.getBlock(castedPacket.posX, castedPacket.posY, castedPacket.posZ);
+
+                    if (ModuleRegistrar.instance().hasNBTProviders(entity) || ModuleRegistrar.instance().hasNBTProviders(teBlock))
+                    {
                         try
                         {
                             NBTTagCompound tag = new NBTTagCompound();
-                            entity.writeToNBT(tag);
+                            tag.setInteger("WailaX", castedPacket.posX);
+                            tag.setInteger("WailaY", castedPacket.posY);
+                            tag.setInteger("WailaZ", castedPacket.posZ);
+
+                            for (Map.Entry<Integer, List<IWailaDataProvider>> entry : ModuleRegistrar.instance().getNBTProviders(teBlock).entrySet())
+                            {
+                                for (IWailaDataProvider provider : entry.getValue())
+                                {
+                                    tag = provider.getNBTData(handler.playerEntity, entity, tag, teWorld, castedPacket.posX, castedPacket.posY, castedPacket.posZ);
+                                }
+                            }
+
+                            for (Map.Entry<Integer, List<IWailaDataProvider>> entry : ModuleRegistrar.instance().getNBTProviders(entity).entrySet())
+                            {
+                                for (IWailaDataProvider provider : entry.getValue())
+                                {
+                                    tag = provider.getNBTData(handler.playerEntity, entity, tag, teWorld, castedPacket.posX, castedPacket.posY, castedPacket.posZ);
+                                }
+                            }
+
                             PacketDispatcher.sendPacketToPlayer(Packet0x02TENBTData.create(tag), handler.playerEntity);
                         }
                         catch (Throwable e)
